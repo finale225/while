fc188a0cf8d15635a7620099edb182a46d5108b5
finale225 <finale2255@naver.com>
2021-06-22 19:22:25 +0900

    A+B_usingEOF

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     int a, b;
     while (true) {
         cin >> a >> b;
-        if (a==0 && b==0) break;
-        cout << a + b << endl;
+        if (cin.eof()) break;
+        cout << a + b << '\n';
     }
 }
