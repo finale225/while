34dbf5058a7c82a717429ed5d7df302361c8f4e9
finale225 <finale2255@naver.com>
2021-06-22 19:40:00 +0900

    plusCycle

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 int main() {
-    int a, b;
+    int n, en, n10, n1, count = 0;
+    cin >> n;
+    en = n;
+    if (n > 99 || n < 0) return 0;
     while (true) {
-        cin >> a >> b;
-        if (cin.eof()) break;
-        cout << a + b << '\n';
+        n10 = n / 10;
+        n1 = n % 10;
+        n = (n1*10) + (n10 + n1) % 10;
+        count++;
+        if (n == en) break;
     }
+    cout << count;
 }
